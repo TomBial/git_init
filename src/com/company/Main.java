498359a3af680d554e5a6559221ba95cbe312b1f
TomBial <57679830+TomBial@users.noreply.github.com>
2019-11-12 19:55:24 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+
 // sout -> System.out.println () ;
 //CTRL + D -> duplikowanie linii kodu
 public class Main {
@@ -6,5 +7,7 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello World!");
 	System.out.println("Publikujemy pierwszy program!!!");
+	    
+	System.out.println("Edycja przez współpracownika!!!");    
     }
 }
